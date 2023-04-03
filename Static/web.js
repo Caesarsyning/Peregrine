d5afdd2f1792aaaa09da55fe8b62fed71613c3d4
Siyuan Ning (Caesar) <107768975+Caesarsyning@users.noreply.github.com>
2023-04-03 00:09:16 -0400

    use bootstrap and add new stylesheet, and vanilla js

--- Static/web.js
+++ Static/web.js
@@ -9,14 +9,33 @@ if (link.href === currentLocation) {
 });
 
 // when scrolling down, the header disappear
-
 var prevScrollpos = window.pageYOffset;
+var timeout = null;
+var isScrolling = false;
+
+// hide the header when scrolling down, show it when scrolling up or not scrolling
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
+    // scrolling up
     document.getElementById("header").style.top = "0";
-  } else {
+    isScrolling = false;
+  } else if (!isScrolling) {
+    // not scrolling or scrolling down
     document.getElementById("header").style.top = "-100px";
+    isScrolling = true;
   }
   prevScrollpos = currentScrollPos;
+}
+
+// bind the showHeader function to mousemove and keydown events
+document.addEventListener("mousemove", showHeader);
+document.addEventListener("keydown", showHeader);
+
+// show the header if the user is not scrolling
+function showHeader() {
+  if (!isScrolling) {
+    document.getElementById("header").style.top = "0";
+    isScrolling = false;
+  }
 }
